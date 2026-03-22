a0e406245fd8ed3c7779727396ab9f2fdf9d7cd0
sheikhsujauddin <suja.ibusiness@gmail.com>
2026-03-22 20:54:40 +0400

    Change include_colab_link value to True

--- fintickai.py
+++ fintickai.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyNPady3THS9es+4lPscj1qD",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
